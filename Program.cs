6078aeb6ed7861b24a6e5b5c91f4ffabc2092963
olafkotur <olafkotur97@gmail.com>
2021-10-01 14:47:32 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_pipeline_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
